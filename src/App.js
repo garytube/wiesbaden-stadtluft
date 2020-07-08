0dda88dab316609d5ee40e5726cf66b02de36261
geryy <g@geryy.io>
2020-07-08 16:22:06 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,35 +1,61 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Banner from "./Banner/Banner";
+import Banner from './components/banner/banner';
+import axios from 'axios';
 
-class App extends Component {
-    apiUrl = 'https://www.stadtluft-anzeiger.de/getTreeLEDAPIv2.php?stationid=136-ringkirche';
-    apiUrlProxied = 'https://cors-anywhere.herokuapp.com/https://www.stadtluft-anzeiger.de/getTreeLEDAPIv2.php?stationid=136-ringkirche';
-    state = {
-        no2: null,
-        time: null,
-    };
+export default function App() {
+  const [no2, setNo2] = useState(null);
+  const [time, setTime] = useState(null);
+  const [error, setError] = useState(null);
+  const apiUrlProxied =
+    'https://cors-anywhere.herokuapp.com/https://www.stadtluft-anzeiger.de/getTreeLEDAPIv2.php?stationid=136-ringkirche';
 
-    resolution = 'fullHD';
-    //resolution = 'ultraHD';
+  useEffect(() => {
+    axios
+      .get(apiUrlProxied)
+      .then(({ data }) => {
+        setNo2(data.no2);
+        setTime(data.tm);
+      })
+      .catch(setError);
+  }, []);
 
-    componentWillMount() {
-        fetch(this.apiUrlProxied)
-        .then(res => res.json())
-        .then(json => this.setState({ no2: json.NO2, time: json.time }))
-        .catch(e => console.log(e))
-    }
+  if (error) return <div>{JSON.stringify(error)}</div>;
 
-    render() {
-        const { no2, time } = this.state;
+  if (!no2 || !time) return <div>loading</div>;
 
-        return (
-            <div className={`App ${this.resolution}`}>
-                {!no2 && <h1 className='loadingTitle'>LOADING...</h1>}
-                <Banner className={this.resolution} time={time} no2={no2} />
-            </div>
-        );
-    }
+  return <Banner time={time} no2={no2} />;
 }
 
-export default App;
+// class App extends Component {
+//   apiUrl = 'https://www.stadtluft-anzeiger.de/getTreeLEDAPIv2.php?stationid=136-ringkirche';
+//   apiUrlProxied = 'https://cors-anywhere.herokuapp.com/https://www.stadtluft-anzeiger.de/getTreeLEDAPIv2.php?stationid=136-ringkirche';
+//   state = {
+//     no2: null,
+//     time: null,
+//   };
+
+//   resolution = 'fullHD';
+//   //resolution = 'ultraHD';
+
+//   componentDidMount() {
+//     fetch(this.apiUrlProxied)
+//       .then((res) => res.json())
+//       .then((json) => {
+//         console.log(json);
+//         this.setState({ no2: json.NO2, time: json.time });
+//       })
+//       .catch((e) => console.log(e));
+//   }
+
+//   render() {
+//     const { no2, time } = this.state;
+//     console.log(this.state);
+//     return (
+//       <div className={`App ${this.resolution}`}>
+//         {!no2 && <h1 className="loadingTitle">LOADING...</h1>}
+//         <Banner className={this.resolution} time={time} no2={no2} />
+//       </div>
+//     );
+//   }
+// }
